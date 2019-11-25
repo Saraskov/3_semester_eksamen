986c7b22c60325558e68af6110e0d7fe6b8897a7
Saraskov <43336948+Saraskov@users.noreply.github.com>
2019-11-25 21:26:03 +0100

    Logo oprettet
    Og andre illustrationer

--- inc/header.php
+++ inc/header.php
@@ -4,8 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Koala games</title>
+    <title>KoalaVerse</title>
     <link rel="stylesheet" href="<?php echo ROOT_URL; ?>stylesheets/master_styles.css">
+    <link rel="icon" href="<?php echo ROOT_URL; ?>illustrationer/koala/koala_logo.png">
 </head>
 <body>
 <?php require('main_nav.php') ?>
--- index.php
+++ index.php
@@ -9,6 +9,9 @@
 <?php include('inc/header.php'); ?>
 
 <h1>Forside</h1>
+<div class="container h-100">
+    <img src="<?php echo ROOT_URL; ?>illustrationer/koala/coverbillede.png">
+</div>
 
 <?php
     session_start();
