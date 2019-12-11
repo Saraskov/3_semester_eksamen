198d859ecffffa128b73b9a3097da617cdde6d66
Saraskov <43336948+Saraskov@users.noreply.github.com>
2019-12-11 13:24:46 +0100

    Logout rettet

--- pages/logout.php
+++ pages/logout.php
@@ -11,7 +11,18 @@
 
 <?php
     session_destroy();
-    alert("du er logget ud");
 ?>
+<div class="container">
+    <div class="row">
+        <div class="col-lg-3 col-sm-0"></div>
+        <div class="col-lg-6 col-sm-12 center">
+            <div class="white gutter">
+                <h3 class="center">Du er ved at logge ud</h3>
+                <button class="btn"><a href="../index.php"><h4>Logout</h4></a></button>
+            </div>
+        </div>
+        <div class="col-lg-3 col-sm-0"></div>
+    </div>
+</div>
 
 <?php include('../inc/footer.php'); ?>
